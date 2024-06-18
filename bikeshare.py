6f50042908999f1e25f51ad3eaf4ef4c161d19c5
Areen <109797692+areenyaran@users.noreply.github.com>
2024-06-18 18:21:53 +0300

    Add another comment in the main

--- bikeshare.py
+++ bikeshare.py
@@ -213,6 +213,7 @@ def main():
         user_stats(df)
         display_raw_data(df)
 
+        # Ask user to restart the process.
         restart = input('\nWould you like to restart? Enter yes or no.\n').strip().lower()
         if restart != 'yes':
             break
